c03c8800f7e64b4d307b4e6a3a49495b0eed46d2
Jeremy Andrews <jeremy@tag1consulting.com>
2019-09-18 15:52:34 +0200

    maintain and expose some global counters

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@
 
 use std::sync::Mutex;
 use std::time::SystemTime;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
-use rocket::State;
-use rocket_contrib::json::{Json, JsonValue};
-use priority_queue::PriorityQueue;
+use rocket::{State, Request, response};
 use rocket::http::{ContentType, Status};
-use rocket::response;
-use rocket::request;
+use rocket_contrib::json::{Json, JsonValue};
+
+
+use priority_queue::PriorityQueue;
 
 type Priority = usize;
 type Timestamp = u128;
@@ -32,11 +33,32 @@ struct MessageInternal {
     arrived: Timestamp,
 }
 type MessageQueue = Mutex<PriorityQueue<MessageInternal, Priority>>;
+
+// Global counters:
+#[derive(Default)]
+struct Counters {
+    queue_requests: AtomicUsize,
+    proxy_requests: AtomicUsize,
+    queued: AtomicUsize,
+    proxied: AtomicUsize,
+    in_queue: AtomicUsize,
+}
+
 // Set an HTTP status when responding with JSON objects
 #[derive(Debug)]
 struct QueueApiResponse {
     json: JsonValue,
     status: Status,
+}
+
+// Customer JSON responder, includes an HTTP status message.
+impl<'r> response::Responder<'r> for QueueApiResponse {
+    fn respond_to(self, req: &Request) -> response::Result<'r> {
+        response::Response::build_from(self.json.respond_to(&req).unwrap())
+            .status(self.status)
+            .header(ContentType::JSON)
+            .ok()
+    }
 }
 
 // Helper function for getting time since the epoch in milliseconds.
@@ -47,19 +69,12 @@ fn time_since_epoch() -> Timestamp {
     }
 }
 
-// Customer JSON responder, includes an HTTP status message.
-impl<'r> response::Responder<'r> for QueueApiResponse {
-    fn respond_to(self, req: &request::Request) -> response::Result<'r> {
-        response::Response::build_from(self.json.respond_to(&req).unwrap())
-            .status(self.status)
-            .header(ContentType::JSON)
-            .ok()
-    }
-}
-
 // Accept incoming messages for the proxy to queue.
 #[post("/", format="json", data="<message>")]
-fn new(message: Json<MessageIn>, queue: State<'_, MessageQueue>) -> QueueApiResponse {
+fn new(message: Json<MessageIn>, queue: State<'_, MessageQueue>, counters: State<Counters>) -> QueueApiResponse {
+    // A POST was routed here, requesting to add something to the queue.
+    let queue_requests = counters.queue_requests.fetch_add(1, Ordering::Relaxed) + 1;
+
     // Priority is optional, set a default if not provided.
     let priority: Priority;
     match message.0.priority {
@@ -80,10 +95,23 @@ fn new(message: Json<MessageIn>, queue: State<'_, MessageQueue>) -> QueueApiResp
     // Grab lock and add message to queue
     let mut messagequeue = queue.lock().expect("queue lock.");
     messagequeue.push(internal, priority);
+    // A message has been sucessfully added to the queue.
+    let queued = counters.queued.fetch_add(1, Ordering::Relaxed) + 1;
+    let in_queue = counters.in_queue.fetch_add(1, Ordering::Relaxed) + 1;
+    // Retreive other debug statistics
+    let proxy_requests = counters.proxy_requests.load(Ordering::Relaxed);
+    let proxied = counters.proxied.load(Ordering::Relaxed);
     QueueApiResponse {
         json: json!({
                 "status": "accepted",
                 "code": 202,
+                "debug": {
+                    "queue_requests": queue_requests,
+                    "proxy_requests": proxy_requests,
+                    "queued": queued,
+                    "proxied": proxied,
+                    "in_queue": in_queue,
+                },
             }),
         status: Status::Accepted,
     }
@@ -91,9 +119,19 @@ fn new(message: Json<MessageIn>, queue: State<'_, MessageQueue>) -> QueueApiResp
 
 // Temporary: ultimately the proxy will push this data.
 #[get("/", format = "json")]
-fn get(queue: State<'_, MessageQueue>) -> Option<QueueApiResponse> {
+fn get(queue: State<'_, MessageQueue>, counters: State<Counters>) -> Option<QueueApiResponse> {
+    // A GET was routed here, requesting to get something from the queue.
+    let proxy_requests = counters.proxy_requests.fetch_add(1, Ordering::Relaxed) + 1;
+
     let mut messagequeue = queue.lock().unwrap();
     messagequeue.pop().map(|internal| {
+        // A message has been sucessfully removed from the queue.
+        let proxied = counters.proxied.fetch_add(1, Ordering::Relaxed) + 1;
+        let in_queue = counters.in_queue.fetch_sub(1, Ordering::Relaxed) - 1;
+        // Retreive other debug statistics
+        let queue_requests = counters.queue_requests.load(Ordering::Relaxed);
+        let queued = counters.queued.load(Ordering::Relaxed);
+
         // Message queue returns a tuple, the internal data strucutre and the priority.
         // Use this to build the JSON response on-the-fly.
         QueueApiResponse {
@@ -104,7 +142,14 @@ fn get(queue: State<'_, MessageQueue>) -> Option<QueueApiResponse> {
                         "contents": internal.0.contents.clone(),
                         "priority": internal.0.priority,
                         "elapsed": (time_since_epoch() - internal.0.arrived) as usize,
-                    }
+                    },
+                    "debug": {
+                        "queue_requests": queue_requests,
+                        "proxy_requests": proxy_requests,
+                        "queued": queued,
+                        "proxied": proxied,
+                        "in_queue": in_queue,
+                    },
                 }),
             status: Status::Ok,
         }
@@ -125,9 +170,10 @@ fn not_found() -> QueueApiResponse {
 
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
-        .mount("/", routes![new, get])
         .register(catchers![not_found])
+        .manage(Counters::default())
         .manage(Mutex::new(PriorityQueue::<MessageInternal, Priority>::new()))
+        .mount("/", routes![new, get])
 }
 
 fn main() {
